fa1ada9997362fdff7333b3d51813657e76d5025
lab661 <lab661@PC-445.urel.feec.vutbr.cz>
2019-12-05 12:49:41 +0100

    create signal SIN, better version

--- projects/10-Generator DTMF (Dual-Tone Multiple Frequencies)/main.c
+++ projects/10-Generator DTMF (Dual-Tone Multiple Frequencies)/main.c
@@ -1,7 +1,6 @@
 /*
  * ---------------------------------------------------------------------
- * Author:      Tomas Fryza
-                Josef HOLUB
+ * Author:      Josef HOLUB
                 Tomas LORENC
  *              Dept. of Radio Electronics, Brno Univ. of Technology
  * Created:     2017-10-27
@@ -9,8 +8,7 @@
  * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
  * ---------------------------------------------------------------------
  * Description:
- *    Scan TWI bus for all connected slave devices and transmit info
- *    to UART.
+ *    
  */
 
 /* Includes ----------------------------------------------------------*/
@@ -31,25 +29,22 @@
 #define sample_rate 16e-6
 
 /* Variables ---------------------------------------------------------*/
-uint16_t i = 0;
-//uint16_t freq = 1000;
-//uint16_t t = 0;
+volatile uint16_t i = 0;
 uint16_t angle = 0;
 uint16_t duty = 0;
-
-unsigned char x;
 volatile unsigned char sine[256];
+
 /* Function prototypes -----------------------------------------------*/
 
 void generate_sin(int freq){  
-    char uart_string[3];
+    //char uart_string[3];
       
     for(unsigned char x=0; x<255;x++){  
     sine[x] = 128*(sin(2.0*M_PI*x*sample_rate*freq) + 1); 
 
-    itoa(sine[x], uart_string, 10);
+    /* itoa(sine[x], uart_string, 10);
     uart_puts(uart_string);
-    uart_puts("\r\n"); 
+    uart_puts("\r\n");  */
     }  
 }
 
@@ -63,24 +58,23 @@ void generate_sin(int freq){
 
 int main(void)
 {
-    //uint16_t x = 0;
-    //char uart_string[3];
-
     uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
 
-    DDRD |= (1<<PD6);
+    GPIO_config_output(&DDRD, PD6);
 
     TCCR0A |= (1 << COM0A1) | (1 << COM0A0) | (1 << WGM00) | (1 << WGM01);  //FAST PWM
-    TCCR0B |= (1 << CS00);                                                  //Presclaler 8
+    TCCR0B |= (1 << CS00);                                                  //Presclaler 0
 
     TIMSK0 |= (1<<TOIE0);                                                   //enable interrupt
 
     sei();
 
-    generate_sin(1000);
+    generate_sin(2000);
 
     while(1){
-        if(i > 256)i = 0;
+        if(i > 249){
+            i = 0;
+        }
     }
 }
 
